7fbeafe4dce00c9ccbaee5432672937efcc0b087
DJ Nadgar <dj@exciteresearch.com>
2015-08-09 02:01:04 -0400

    updated watch page

--- browser/js/about/about.js
+++ browser/js/about/about.js
@@ -1,17 +1,46 @@
 app.config(function ($stateProvider) {
 
     // Register our *about* state.
-    $stateProvider.state('about', {
-        url: '/about',
-        controller: 'AboutController',
+    $stateProvider.state('watch', {
+        url: '/watch',
+        controller: 'WatchController',
         templateUrl: 'js/about/about.html'
     });
 
 });
 
-app.controller('AboutController', function ($scope, FullstackPics) {
+app.controller('WatchController', function ($scope, FullstackPics) {
+	$scope.watch = false;
+	$scope.location = {};
+	$scope.currentLocation = function(){
+		navigator.geolocation.getCurrentPosition(function(position){
+			$scope.location.latitude = position.coords.latitude;
+            $scope.location.longitude = position.coords.longitude;
+            console.log('LOCATION:', $scope.location);
+            return $scope.location;
+		})
+	}
+	$scope.getGeo = function (){
+        if(navigator.geolocation){
+        	$scope.currentLocation();
+        }
+        else {
+          alert("Geolocation is not supported by this browser");
+        }
+    };
+    
+    $scope.currentLocation();
 
-    // Images of beautiful Fullstack people.
-    $scope.images = _.shuffle(FullstackPics);
-
+    $scope.guard = function(){
+    	$scope.watch = true;
+    	$scope.getGeo();
+    	$scope.initialLocation = $scope.location;
+    	$scope.currentLocation();
+    	while($scope.watch){
+	    	if($scope.location !== $scope.initialLocation){
+	    		alert('Geolocation has changed!');
+	    		return $scope.location;
+	    	};
+    	}
+    }
 });
