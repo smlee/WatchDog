2e8ed345ed851d31a10157819ed2f4f4fc6f54e4
smlee <sangmlee23@gmail.com>
2015-08-09 07:27:54 -0400

    fixed 3 warnings

--- browser/js/about/about.js
+++ browser/js/about/about.js
@@ -32,121 +32,121 @@ app.controller('WatchController', function ($scope, FullstackPics) {
         img.src = canvas.toDataURL();
         capturebutton.appendChild(img);
     };
-/*
-    var video, width, height, context;
-    var bufidx = 0, buffers = [];*/
 
 
     /**
      * Based on https://timtaubert.de/demos/motion-detection/
      */
 
-        var video, width, height, context;
-        var bufidx = 0, buffers = [];
+    var video, width, height, context;
+    var bufidx = 0, buffers = [];
 
-        function initialize() {
-            // The source video.
-            video = document.getElementById("v");
-            width = video.width;
-            height = video.height;
 
-            // The target canvas.
-            var canvas = document.getElementById("c");
-            console.log(canvas);
-            context = canvas.getContext("2d");
 
-            // Prepare buffers to store lightness data.
-            for (var i = 0; i < 2; i++) {
-                buffers.push(new Uint8Array(width * height));
-            }
-
-            // Get the webcam's stream.
-            navigator.getUserMedia({video: true}, startStream, function () {});
+    function readFrame() {
+        try {
+            context.drawImage(video, 0, 0, width, height);
+        } catch (e) {
+            // The video may not be ready, yet.
+            return null;
         }
 
+        return context.getImageData(0, 0, width, height);
+    }
 
-        function startStream(stream) {
-            video.src = URL.createObjectURL(stream);
-            video.play();
+    function draw() {
+        var frame = readFrame();
 
-            // Ready! Let's start drawing.
-            requestAnimationFrame(draw);
+        if (frame) {
+            markLightnessChanges(frame.data);
+            context.putImageData(frame, 0, 0);
         }
 
-        function draw() {
-            var frame = readFrame();
+        // Wait for the next frame.
+        requestAnimationFrame(draw);
+    }
 
-            if (frame) {
-                markLightnessChanges(frame.data);
-                context.putImageData(frame, 0, 0);
-            }
+    function startStream(stream) {
+        video.src = URL.createObjectURL(stream);
+        video.play();
 
-            // Wait for the next frame.
-            requestAnimationFrame(draw);
+        // Ready! Let's start drawing.
+        requestAnimationFrame(draw);
+    }
+
+
+    function initialize() {
+        // The source video.
+        video = document.getElementById("v");
+        width = video.width;
+        height = video.height;
+
+        // The target canvas.
+        var canvas = document.getElementById("c");
+        console.log(canvas);
+        context = canvas.getContext("2d");
+
+        // Prepare buffers to store lightness data.
+        for (var i = 0; i < 2; i++) {
+            buffers.push(new Uint8Array(width * height));
         }
 
-        function readFrame() {
-            try {
-                context.drawImage(video, 0, 0, width, height);
-            } catch (e) {
-                // The video may not be ready, yet.
-                return null;
-            }
+        // Get the webcam's stream.
+        navigator.getUserMedia({video: true}, startStream, function () {});
+    }
 
-            return context.getImageData(0, 0, width, height);
-        }
 
-        function markLightnessChanges(data) {
-            // Pick the next buffer (round-robin).
-            var buffer = buffers[bufidx++ % buffers.length];
-            var h = 0;
-            var l = 0;
+    function markLightnessChanges(data) {
+        // Pick the next buffer (round-robin).
+        var buffer = buffers[bufidx++ % buffers.length];
+        var h = 0;
+        var l = 0;
 
-            for (var i = 0, j = 0; i < buffer.length; i++, j += 4) {
-                // Determine lightness value.
-                var current = lightnessValue(data[j], data[j + 1], data[j + 2]);
+        for (var i = 0, j = 0; i < buffer.length; i++, j += 4) {
+            // Determine lightness value.
+            var current = lightnessValue(data[j], data[j + 1], data[j + 2]);
 
-                // Set color to black.
-                data[j] = data[j + 1] = data[j + 2] = 0;
+            // Set color to black.
+            data[j] = data[j + 1] = data[j + 2] = 0;
 
-                // Full opacity for changes.
-                data[j + 3] = 255 * lightnessHasChanged(i, current);
-                if (lightnessHasChanged(i, current) && isGaurding){
-                    if (current > 70) {
-                        //start counting high risk activities
-                        h++;
-                        console.log("h",h);
+            // Full opacity for changes.
+            data[j + 3] = 255 * lightnessHasChanged(i, current);
+            if (lightnessHasChanged(i, current) && isGaurding){
+                if (current > 70) {
+                    //start counting high risk activities
+                    h++;
+                    console.log("h",h);
 
-                        //if there is too much high risk triggers send phone call and text message to user
-                        if (h > 200) console.log('someone is wandering around your computer', new Date());
+                    //if there is too much high risk triggers send phone call and text message to user
+                    if (h > 200) console.log('someone is wandering around your computer', new Date());
 
-                    } else {
-                        //start counting low risk triggers
-                        l++;
+                } else {
+                    //start counting low risk triggers
+                    l++;
 
-                        //if there are over 500 low risk triggers send text to user
-                        if (l > 500){
-                            console.log('suspicious activing going on.', new Date())
-                        }
-
+                    //if there are over 500 low risk triggers send text to user
+                    if (l > 500){
+                        console.log('suspicious activing going on.', new Date())
                     }
+
                 }
-
-                // Store current lightness value.
-                buffer[i] = current;
             }
-        }
 
-        function lightnessHasChanged(index, value) {
-            return buffers.some(function (buffer) {
-                return Math.abs(value - buffer[index]) >= 50; //15 is original value
-            });
+            // Store current lightness value.
+            buffer[i] = current;
         }
+    }
 
-        function lightnessValue(r, g, b) {
-            return (Math.min(r, g, b) + Math.max(r, g, b)) / 255 * 50;
-        }
+    function lightnessHasChanged(index, value) {
+        return buffers.some(function (buffer) {
+            return Math.abs(value - buffer[index]) >= 50; //15 is original value
+        });
+    }
 
-       initialize();
+    function lightnessValue(r, g, b) {
+        return (Math.min(r, g, b) + Math.max(r, g, b)) / 255 * 50;
+    }
+
+    initialize();
 
 });
