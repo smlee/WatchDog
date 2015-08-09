c467b0bd3991e06295300a5700f0f29bc3efa233
smlee <sangmlee23@gmail.com>
2015-08-09 02:53:01 -0400

    added testing button

--- browser/js/about/about.js
+++ browser/js/about/about.js
@@ -1,17 +1,32 @@
 app.config(function ($stateProvider) {
 
     // Register our *about* state.
-    $stateProvider.state('about', {
-        url: '/about',
-        controller: 'AboutController',
+    $stateProvider.state('watch', {
+        url: '/watch',
+        controller: 'WatchController',
         templateUrl: 'js/about/about.html'
     });
 
 });
 
-app.controller('AboutController', function ($scope, FullstackPics) {
+app.controller('WatchController', function ($scope, FullstackPics) {
 
     // Images of beautiful Fullstack people.
     $scope.images = _.shuffle(FullstackPics);
+
+    $scope.capture = function(){
+        var video = document.getElementsByClassName('webcam-live')[0];
+        var capturebutton = document.getElementById('capture-button');
+
+        var canvas = document.createElement("canvas");
+        canvas.width = video.videoWidth;
+        canvas.height = video.videoHeight;
+        canvas.getContext('2d')
+            .drawImage(video, 0, 0, canvas.width, canvas.height);
+
+        var img = document.createElement("img");
+        img.src = canvas.toDataURL();
+        capturebutton.appendChild(img);
+    };
 
 });
